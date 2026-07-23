7b1931433f3a07ae947295f16e4726c82ff457af
Harsh Singh <hs1663531@gmail.com>
2026-07-23 10:08:06 +0530

    kitbash 0.8.1

--- Formula/kitbash.rb
+++ Formula/kitbash.rb
@@ -1,8 +1,8 @@
 class Kitbash < Formula
   desc "Package manager and compiler for AI agent skills"
   homepage "https://singhharsh1708.github.io/kitbash/"
-  url "https://registry.npmjs.org/kitbash/-/kitbash-0.8.0.tgz"
-  sha256 "d8b6f6264bd80ed6a8890c1ca4b3210804c379a269271f023ca8366989eac3f6"
+  url "https://registry.npmjs.org/kitbash/-/kitbash-0.8.1.tgz"
+  sha256 "ad7ac1fd06d8de0654a968881294066327bd6b6973ccc293527b1ee20a959122"
   license "Apache-2.0"
 
   depends_on "node"
